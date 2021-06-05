f76c65267d07c06eed8d00d689844b8f70bf729b
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-06-05 18:08:07 -0500

    Se edita compoenete de infoDia

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -40,49 +40,13 @@ export const Home = () => {
                         </form>
                     </div>
                     <div className="col-md-6 d-flex flex-column justify-content-center">
-                        <InfoDia />
+                        <InfoDia tipoDia="dia" />
                         <hr />
                         <div className="d-flex justify-content-around">
-                            {/* <div className="d-flex flex-column align-items-center">
-                                <div className="d-flex flex-column align-items-center">
-                                    <FontAwesomeIcon icon={faCloud} className={ color2 } />
-                                    <p>Lunes</p>
-                                </div>
-                                <div className="d-flex align-items-center">
-                                    <p className="temp-maxima">30°C</p>
-                                    <p className="temp-minima align-bottom">15°C</p>
-                                </div>
-                            </div>
-                            <div className="d-flex flex-column align-items-center">
-                                <div className="d-flex flex-column align-items-center">
-                                    <FontAwesomeIcon icon={faCloud} className={ color2 } />
-                                    <p>Martes</p>
-                                </div>
-                                <div className="d-flex align-items-center">
-                                    <p className="temp-maxima">30°C</p>
-                                    <p className="temp-minima align-bottom">15°C</p>
-                                </div>
-                            </div>
-                            <div className="d-flex flex-column align-items-center">
-                                <div className="d-flex flex-column align-items-center">
-                                    <FontAwesomeIcon icon={faCloud} className={ color2 } />
-                                    <p>Miercoles</p>
-                                </div>
-                                <div className="d-flex align-items-center">
-                                    <p className="temp-maxima">30°C</p>
-                                    <p className="temp-minima align-bottom">15°C</p>
-                                </div>
-                            </div>
-                            <div className="d-flex flex-column align-items-center">
-                                <div className="d-flex flex-column align-items-center">
-                                    <FontAwesomeIcon icon={faCloud} className={ color2 } />
-                                    <p>Jueves</p>
-                                </div>
-                                <div className="d-flex align-items-center">
-                                    <p className="temp-maxima">30°C</p>
-                                    <p className="temp-minima align-bottom">15°C</p>
-                                </div>
-                            </div> */}
+                            <InfoDia tipoDia="semana" />
+                            <InfoDia tipoDia="semana" />
+                            <InfoDia tipoDia="semana" />
+                            <InfoDia tipoDia="semana" />
                         </div>
                     </div>
                 </div>
--- src/components/infoDia/InfoDia.jsx
+++ src/components/infoDia/InfoDia.jsx
@@ -26,10 +26,19 @@ export const InfoDia = ({ tipoDia }) => {
         <div className="d-flex flex-column align-items-center">
             <div className="d-flex flex-column align-items-center">
                 <FontAwesomeIcon icon={ icono } className={ color } />
-                <p className="temp-actual">30°C</p>
-                <p className="tiempo">Nublado</p>
+
+                { tipoDia == 'dia' ? 
+                    <>
+                        <p className="temp-actual">30°C</p>
+                        <p className="tiempo">Nublado</p>
+                    </>
+                    :
+                    <p>Lunes</p>
+                }
             </div>
-            <p className="ciudad">Durango, Dgo.</p>
+
+            { tipoDia == 'dia' && <p className="ciudad">Durango, Dgo.</p> }
+
             <div className="d-flex align-items-center">
                 <p className="temp-maxima">30°C</p>
                 <p className="temp-minima align-bottom">15°C</p>
@@ -37,16 +46,3 @@ export const InfoDia = ({ tipoDia }) => {
         </div>
     )
 }
-
-/*versión de los días de la semana
-    <div className="d-flex flex-column align-items-center">
-        <div className="d-flex flex-column align-items-center">
-            <FontAwesomeIcon icon={faCloud} className={ color2 } />
-            <p>Lunes</p>
-        </div>
-        <div className="d-flex align-items-center">
-            <p className="temp-maxima">30°C</p>
-            <p className="temp-minima align-bottom">15°C</p>
-        </div>
-    </div>
-*/
